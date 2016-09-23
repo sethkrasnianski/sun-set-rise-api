e6662ddc793b9b483df1ceafbf9c82211303c026
Seth Krasnianski <sethkrasnianski@gmail.com>
2016-09-23 17:14:30 -0400

    Have root respond with sun information

--- server.cr
+++ server.cr
@@ -1,10 +1,12 @@
 require "json"
 require "kemal"
+require "./middleware/weather_request"
 
 get "/" do |env|
   env.response.content_type = "application/json"
-  {test: "works"}.to_json
+  env.sun.to_json
 end
 
+Kemal.config.add_handler WeatherRequest.new
 Kemal.config.port = 3838
 Kemal.run
